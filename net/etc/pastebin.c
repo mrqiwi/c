d4e1b7573359b95767b00e7b6b0d24adcba332b6
mrqiwi <mrqiwi@ya.ru>
2020-11-06 09:05:03 +0300

    fix work of pastebin

--- net/etc/pastebin.c
+++ net/etc/pastebin.c
@@ -15,7 +15,7 @@
                 "&api_dev_key=%s" \
                 "&api_paste_code=%s"
 
-static char *generate_request(char *filename)
+static char *generate_request(const char *data, const char *filename)
 {
     char *filetype = strrchr(filename, '.');
     if (filetype)
@@ -23,6 +23,21 @@ static char *generate_request(char *filename)
     else
         filetype = "";
 
+    int length = snprintf(NULL, 0, PATTERN, filetype, getenv("PASTEBIN"), data) + 1;
+
+    char *buffer = malloc(length);
+    if (!buffer) {
+        perror("malloc() failed");
+        return NULL;
+    }
+
+    snprintf(buffer, length, PATTERN, filetype, getenv("PASTEBIN"), data);
+
+    return buffer;
+}
+
+static char *url_encoded_string(CURL *curl, const char *filename)
+{
     int fd = open(filename, O_RDONLY);
     if (fd < 0) {
         perror("open() failed");
@@ -43,35 +58,40 @@ static char *generate_request(char *filename)
         return NULL;
     }
 
-    int length = snprintf(NULL, 0, PATTERN, filetype, getenv("PASTEBIN"), buffer) + 1;
-
-    char *data = malloc(length);
-    if (!data) {
-        close(fd);
-        munmap(buffer, size);
-        perror("malloc() failed");
-        return NULL;
-    }
-
-    snprintf(data, length, PATTERN, filetype, getenv("PASTEBIN"), buffer);
+    char *encoded = curl_easy_escape(curl, buffer, strlen(buffer));
 
     close(fd);
     munmap(buffer, size);
 
-    return data;
+    return encoded;
 }
 
-int send_request(char *data)
+int send_request(char *filename)
 {
     CURL *curl = curl_easy_init();
-    if (!curl)
+    if (!curl) {
+        puts("cannot init curl");
         return -1;
+    }
+
+    char *encoded = url_encoded_string(curl, filename);
+    if (!encoded) {
+        puts("cannot encode string");
+        return -1;
+    }
+
+    char *request = generate_request(encoded, filename);
+    curl_free(encoded);
+
+    if (!request) {
+        puts("cannot generate request");
+        return -1;
+    }
 
     curl_easy_setopt(curl, CURLOPT_URL, "https://pastebin.com/api/api_post.php");
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "pastebin 1.0");
     curl_easy_setopt(curl, CURLOPT_POST, 1L);
-    //NOTE некорректный размер Content-Lenght, не понятно почему
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE_LARGE, strlen(data));
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, request);
     /* curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L); */
 
     CURLcode res = curl_easy_perform(curl);
@@ -107,16 +127,7 @@ int main(int argc, char **argv)
         };
     };
 
-    char *data = generate_request(filename);
-    if (data == NULL) {
-        puts("cannot generate request");
-        return -1;
-    }
-
-    res = send_request(data);
-    free(data);
-
-    if (res < 0) {
+    if (send_request(filename) < 0) {
         puts("cannot send request");
         return -1;
     }
